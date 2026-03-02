dcd51f0954954e680f345a13470c37e4523cbc94
reinoutkoninkx <ron89king@gmail.com>
2026-03-02 15:54:49 +0100

    plans controller spec create plan

--- spec/controllers/api/v1/plans_controller_spec.rb
+++ spec/controllers/api/v1/plans_controller_spec.rb
@@ -30,69 +30,9 @@ RSpec.describe 'Api::V1::Plans', type: :request do
   end
 
   describe 'GET /api/v1/plans' do
-    # create plan endpoint
-    # it 'creates a new plan' do
-    #   plan_params = {
-    #     total_items: 1,
-    #     items: [
-    #       {
-    #         dmp: {
-    #           title: 'Examination of some interesting topics in biochemistry',
-    #           contact: {
-    #             name: 'Jane Doe',
-    #             mbox: 'jane.doe@example.edu',
-    #             affiliation: { name: 'Example University' },
-    #             contact_id: { type: 'orcid', identifier: '0000-0000-0000-0000' }
-    #           },
-    #           contributor: [
-    #             {
-    #               name: 'John Smith',
-    #               mbox: 'john.smith@un.edu',
-    #               role: [
-    #                 'https://dictionary.casrai.org/Contributor_Roles/Data_curation',
-    #                 'https://dictionary.casrai.org/Contributor_Roles/Investigation'
-    #               ],
-    #               affiliation: {
-    #                 name: 'University of Nowhere',
-    #                 affiliation_id: { type: 'ror', identifier: 'https://ror.org/123abc45y' }
-    #               },
-    #               contributor_id: { type: 'orcid', identifier: 'https://orcid.org/0000-0000-0000-0000' }
-    #             }
-    #           ],
-    #           extension: [
-    #             {
-    #               dmproadmap: {
-    #                 template: { id: template.id }
-    #               }
-    #             }
-    #           ]
-    #         }
-    #       }
-    #     ]
-    #   }
-
-    #   headers = {
-    #     'Content-Type' => 'application/json',
-    #     'Accept' => 'application/json',
-    #     'Server-Agent' => 'DMPonline',
-    #     'Authorization' => "Bearer #{access_token}"
-    #   }
-
-    #   post '/api/v1/plans', params: plan_params.to_json, headers: headers
-
-    #   expect(response).to have_http_status(:created)
-
-    #   parsed_response = JSON.parse(response.body)
-    #   expect(parsed_response['items'].first['dmp']['title']).to eq('Examination of some interesting topics in biochemistry')
-
-    #   expect(Plan.exists?(title: 'Examination of some interesting topics in biochemistry')).to be true
-    # end
-
     it 'returns a list of plans' do
       get '/api/v1/plans', headers: { 'Authorization' => "Bearer #{access_token}" }
 
-      puts response.body
-      puts "plan: #{plan.inspect}"
       expect(response).to have_http_status(:ok)
       parsed_response = JSON.parse(response.body)
       expect(parsed_response).to be_a(Hash)
@@ -103,6 +43,62 @@ RSpec.describe 'Api::V1::Plans', type: :request do
       get "/api/v1/plans/#{plan.id}", headers: { 'Authorization' => "Bearer #{access_token}" }
 
       expect(response).to have_http_status(:ok)
+    end
+
+    # create plan endpoint
+    it 'creates a new plan' do
+      plan_params = {
+        total_items: 1,
+        items: [
+          {
+            dmp: {
+              title: 'Examination of some interesting topics in biochemistry',
+              contact: {
+                name: 'Jane Doe',
+                mbox: 'jane.doe@example.edu',
+                affiliation: { name: 'Example University' },
+                contact_id: { type: 'orcid', identifier: '0000-0000-0000-0000' }
+              },
+              contributor: [
+                {
+                  name: 'John Smith',
+                  mbox: 'john.smith@un.edu',
+                  role: [
+                    'https://dictionary.casrai.org/Contributor_Roles/Data_curation',
+                    'https://dictionary.casrai.org/Contributor_Roles/Investigation'
+                  ],
+                  affiliation: {
+                    name: 'University of Nowhere',
+                    affiliation_id: { type: 'ror', identifier: 'https://ror.org/123abc45y' }
+                  },
+                  contributor_id: { type: 'orcid', identifier: 'https://orcid.org/0000-0000-0000-0000' }
+                }
+              ],
+              extension: [
+                {
+                  dmproadmap: {
+                    template: { id: template.id }
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+
+      headers = {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+        'Authorization' => "Bearer #{access_token}"
+      }
+
+      post '/api/v1/plans', params: plan_params.to_json, headers: headers
+
+      puts "Response: #{response.inspect}"
+      expect(response).to have_http_status(:created)
+
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response['items'].first['dmp']['title']).to eq('Examination of some interesting topics in biochemistry')
     end
   end
 end
