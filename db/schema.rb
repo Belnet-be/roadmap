3108eca45560763dc2c319c7c3a94399721df53f
Patrik Schrey <patrik.schrey@telenet.be>
2026-06-26 11:37:37 +0200

    revert schema.rb changes for spr0014.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_26_115825) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_02_151604) do
 # Could not dump table "annotations" because of following ActiveRecord::StatementInvalid
 #   Mysql2::Error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'NULL' at line 1
 
@@ -25,12 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_26_115825) do
   end
 
 # Could not dump table "api_clients" because of following ActiveRecord::StatementInvalid
-#   Mysql2::Error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'NULL' at line 1
-
-# Could not dump table "belnet_stage_histories" because of following ActiveRecord::StatementInvalid
-#   Mysql2::Error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'NULL' at line 1
-
-# Could not dump table "belnet_stages" because of following ActiveRecord::StatementInvalid
 #   Mysql2::Error: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'NULL' at line 1
 
 # Could not dump table "conditions" because of following ActiveRecord::StatementInvalid
@@ -250,10 +244,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_26_115825) do
   add_foreign_key "answers", "users"
   add_foreign_key "answers_question_options", "answers"
   add_foreign_key "answers_question_options", "question_options"
-  add_foreign_key "belnet_stage_histories", "belnet_stages"
-  add_foreign_key "belnet_stage_histories", "plans"
-  add_foreign_key "belnet_stage_histories", "users"
-  add_foreign_key "belnet_stages", "orgs"
   add_foreign_key "conditions", "questions"
   add_foreign_key "guidance_groups", "orgs"
   add_foreign_key "guidances", "guidance_groups"
@@ -266,7 +256,6 @@ ActiveRecord::Schema[7.1].define(version: 2026_05_26_115825) do
   add_foreign_key "orgs", "languages"
   add_foreign_key "orgs", "regions"
   add_foreign_key "phases", "templates"
-  add_foreign_key "plans", "belnet_stages"
   add_foreign_key "plans", "orgs"
   add_foreign_key "plans", "templates"
   add_foreign_key "plans_guidance_groups", "guidance_groups"
