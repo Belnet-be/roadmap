122b1638fc823987973c7a2ddf16064f611ef685
Patrik Schrey <patrik.schrey@telenet.be>
2026-09-23 16:36:08 +0200

    test email forwarding

--- config/initializers/contact_us_local.rb
+++ config/initializers/contact_us_local.rb
@@ -4,7 +4,7 @@
 # for more information
 ContactUs.setup do |config|
   config.mailer_from = "dmponline@belnet.be"
-  config.mailer_to = "adminsupport@belnet.be"
+  config.mailer_to = "patrik.schrey@belnet.be"
   config.require_name = true
   config.require_subject = true
   config.localize_routes = false
--- config/initializers/devise_ugent.rb
+++ config/initializers/devise_ugent.rb
@@ -4,7 +4,7 @@ ENV["BASE_URL"] ||= "http://localhost:3000"
 
 # rubocop:disable Metrics/BlockLength
 Devise.setup do |config|
-  config.mailer_sender = "adminsupport@belnet.be"
+  config.mailer_sender = "patrik.schrey@belnet.be"
   OmniAuth.config.full_host = ENV["BASE_URL"]
   config.omniauth(
     :orcid,
